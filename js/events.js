af9413c9b83d207c55f6529652c5eb3c796e22f4
Firat Aksu <aksu_fr@yahoo.es>
2018-03-18 07:14:11 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,5 +9,11 @@ getIt()
 function getIt(){
   $('p').on('click', function(){
     alert("Hey!")
-  })
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass("tasty")
+  });
 }
