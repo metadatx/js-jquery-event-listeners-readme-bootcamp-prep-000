ed20a1422894a543ddd6c87ba6ac6833efb95601
Firat Aksu <aksu_fr@yahoo.es>
2018-03-18 07:32:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,33 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
+
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $('input:first').on('keydown', function(e){
+    if(e.which == 71)
+      alert("G is pressed");
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
